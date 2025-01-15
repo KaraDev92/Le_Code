83dd5d488a04d0f1928886638ff6921f2489c714
KaraDev <al.charlesjoffredo@outlook.fr>
2025-01-15 18:16:46 +0100

    Un peu de css et favicon

--- leFront/divineClub/src/app.component.ts
+++ leFront/divineClub/src/app.component.ts
@@ -5,9 +5,9 @@ import { RouterOutlet } from '@angular/router';
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
-  templateUrl: './app.component.html',
+  templateUrl: './app/pages/home/app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'divineClub';
+  title = 'Divine Club';
 }
